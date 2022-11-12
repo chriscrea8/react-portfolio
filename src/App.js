c3f9860676b25a2da2d18c63995805bd6cb02e8d
chriscrea8 <christianchukwu84@gmail.com>
2022-11-12 17:07:54 +0100

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-=======
 import './App.css'
 import Header from './components/header/Header'
 import About from './components/about/About'
@@ -12,32 +8,10 @@ import Portfolio from './components/portfolio/Portfolio'
 import Services from './components/services/Services'
 import Nav from './components/nav/Nav'
 import Testimonials from './components/testimonials/Testimonials'
->>>>>>> 110fd113735ea1f1b430fb4574ad56f057aaa7c1
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-=======
       <>
         <Header />
         <Nav />
@@ -54,4 +28,3 @@ export default App;
 }
 
 export default App
->>>>>>> 110fd113735ea1f1b430fb4574ad56f057aaa7c1
--- src/index.js
+++ src/index.js
@@ -1,33 +1,15 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-=======
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
->>>>>>> 110fd113735ea1f1b430fb4574ad56f057aaa7c1
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-<<<<<<< HEAD
-);
-=======
 )
->>>>>>> 110fd113735ea1f1b430fb4574ad56f057aaa7c1
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-<<<<<<< HEAD
-reportWebVitals();
-=======
->>>>>>> 110fd113735ea1f1b430fb4574ad56f057aaa7c1
