cf17101e049663bcda1af103702c2d2499420d54
chriscrea8 <christianchukwu84@gmail.com>
2022-12-05 19:49:25 +0100

    created and styled header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import CTA from './CTA'
+import ME from '../../assets/me2.png'
+import HeaderSocials from './HeaderSocials'
+import './Header.css'
 
 const Header = () => {
   return (
-    <div>Header component</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Christian Chukwu</h1>
+        <h5 className='text-light'>Frontend Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'> Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
